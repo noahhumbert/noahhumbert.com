f0179eb62b6b37fd3daca10db9a198fa16049f92
Noah <nhumbert26@gmail.com>
2025-11-16 23:55:30 +0000

    Added a twig template to my Hello World page

---
+++ src/Controller/TestController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/TestController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TestController extends AbstractController {
+     #[Route('/test')]
+     public function test(): Response {
+	  $hello = 'Hello World';
+
+          return $this->render('test.html.twig', [
+               'hello' => $hello,
+          ]);
+     }
+}
+
+?>
