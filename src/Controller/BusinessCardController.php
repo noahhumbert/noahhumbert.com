2b595aab48b344d511333183edb8a0e8c4a62d2f
Noah Humbert <123339506+noahhumbert@users.noreply.github.com>
2025-11-22 10:47:08 -0500

    bug fix

--- src/Controller/BusinessCardController.php
+++ src/Controller/BusinessCardController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ResumeController extends AbstractController {
+class BusinessCardController extends AbstractController {
     #[Route('/business-card', name: 'business-card')]
     public function resume(): Response {
         return $this->render('business-card.html.twig');
