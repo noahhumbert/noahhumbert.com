296af046002074a68dd862e40da47675f9a58503
Noah Humbert <123339506+noahhumbert@users.noreply.github.com>
2025-11-23 09:14:20 -0500

    Staged Home controller and twig file

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+// /src/Controller/HomeController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController {
+    #[Route('/', name: 'home')]
+    public function resume(): Response {
+        return $this->render('home.html.twig');
+    }
+}
+
+?>
