f2fc07685dbc063f71cd4732048e1694fa70f11b
Noah Humbert <123339506+noahhumbert@users.noreply.github.com>
2025-12-23 18:01:14 -0500

    Fixed sendfrom

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -43,7 +43,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('verification@noahhumbert.com', 'Verification'))
+                    ->from(new Address('noreply@noahhumbert.com', 'No Reply'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
