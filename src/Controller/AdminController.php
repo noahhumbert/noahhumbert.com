6ca080175e159c44a3c8d6bbf0749e622bdafbc6
Noah Humbert <123339506+noahhumbert@users.noreply.github.com>
2025-12-25 10:18:22 -0500

    added remove role

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\AddRoleType;
+use App\Form\RemoveRoleType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +19,7 @@ class AdminController extends AbstractController
         Request $request,
         EntityManagerInterface $em
     ): Response {
+        /** ADD ROLE FORM */
         $form = $this->createForm(AddRoleType::class);
         $form->handleRequest($request);
 
@@ -44,6 +46,36 @@ class AdminController extends AbstractController
             }
 
             return $this->redirectToRoute('admin');
+        }
+
+        /** REMOVE ROLE FORM */
+        $removeRoleForm = $this->createForm(RemoveRoleType::class);
+        $removeRoleForm->handleRequest($request);
+
+        if ($removeRoleForm->isSubmitted() && $removeRoleForm->isValid()) {
+            $data  = $removeRoleForm->getData();
+            $email = $data['email'];
+            $role  = $data['role'];
+
+            $user = $em->getRepository(User::class)->findOneBy(['email' => $email]);
+
+            if ($user) {
+                $roles = array_diff($user->getRoles(), [$role]);
+
+                // Symfony requires ROLE_USER at minimum
+                if (empty($roles)) {
+                    $roles = ['ROLE_USER'];
+                }
+
+                $user->setRoles($roles);
+                $em->flush();
+
+                $this->addFlash('success', 'Role removed.');
+            } else {
+                $this->addFlash('danger', 'User not found.');
+            }
+
+            return $this->redirectToRoute('admin_dashboard');
         }
 
         return $this->render('admin.html.twig', [
