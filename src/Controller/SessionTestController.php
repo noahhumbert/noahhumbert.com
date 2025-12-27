6525202d10c45662ee7589cd1d9189c6207ce25d
Noah Humbert <123339506+noahhumbert@users.noreply.github.com>
2025-12-27 09:31:54 -0500

    Update SessionTestController.php

--- src/Controller/SessionTestController.php
+++ src/Controller/SessionTestController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
