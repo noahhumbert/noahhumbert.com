9a95abefc32efc1f7e56ece51cbbd5be18848ca2
Noah Humbert <123339506+noahhumbert@users.noreply.github.com>
2025-12-28 09:36:16 -0500

    added request symfony dependency

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Attribute\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TestController extends AbstractController {
