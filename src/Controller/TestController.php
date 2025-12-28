98a43012c907b5e9e8929c2cf2d70c7d896099ab
Noah Humbert <123339506+noahhumbert@users.noreply.github.com>
2025-12-28 09:38:31 -0500

    corrected request depency path

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Routing\Attribute\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TestController extends AbstractController {
